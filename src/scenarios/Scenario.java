e05f5b52fb7b08a4b0feb69cb242e9bcb5358aca
BotRamenRobotics <redstonegamer360@gmail.com>
2022-05-13 11:20:04 -0400

    Little added to Scenario

--- src/scenarios/Scenario.java
+++ src/scenarios/Scenario.java
@@ -3,8 +3,12 @@ package scenarios;
 public class Scenario {
 
     private String setting; //Name of the place this scenario takes place
+    private Character girl;
+    //Another variable for options?
     
     public Scenario(String setting) {
         this.setting = setting;
     }
+
+    
 }
