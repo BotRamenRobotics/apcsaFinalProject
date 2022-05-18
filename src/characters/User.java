e3ed2138a8d4ed320c5e607d92cab7b756387d55
BotRamenRobotics <redstonegamer360@gmail.com>
2022-05-18 11:57:55 -0400

    Added inventory

--- src/characters/User.java
+++ src/characters/User.java
@@ -5,6 +5,8 @@ public class User {
     private String name;
     private int money;
     private static int energy;
+    private String[] inven;
+    private int space;
 
 
     public User(String name) {
@@ -12,6 +14,9 @@ public class User {
         this.name = name;
         money = 0;
         energy = 6;
+        inven = new String[6];
+        space = 6; 
+
     }
 
     public int getMoney() {
@@ -29,6 +34,37 @@ public class User {
     // getenrgy do that later maybe please 
     public static int getEnergy() {
         return energy;
+    }
+    public int getInventorySpace() {
+        return space;
+    }
+    public String searchItem(String item) {
+
+        boolean found = false;
+
+        for (int i = 0; i < inven.length; i++) {
+            if (inven[i].equals(item)) {
+                found = true;
+                break;
+            }
+        }
+        if (found) 
+        return "You have this item";
+        else
+        return "You dont have this item";
+    }
+    public boolean gift(String item) {
+
+        boolean found = false;
+
+        for (int i = 0; i < inven.length; i++) {
+            if (inven[i].equals(item)) {
+                found = true;
+                inven[i] = null;
+                break;
+            }
+        }
+        return found;
     }
     
 }
